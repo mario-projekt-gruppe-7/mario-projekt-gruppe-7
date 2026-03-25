68a3c0f7287c6f99ce88f5b7e653cb28cef69433
Carl Emil Zeidler <carlemilzeidler@gmail.com>
2026-03-25 21:33:40 +0100

    feature_Controller

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.bygStandardMenu();
-        UI ui = new UI(menu);
-        ui.mainMenu();// systemet starter i main menu
+        Controller controller = new Controller(menu);
+        controller.start();
     }
 }
