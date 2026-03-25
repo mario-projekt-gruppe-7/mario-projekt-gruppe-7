0bdc09f720d51b0ea37a11b857a4c4fa11933c70
Carl Emil Zeidler <carlemilzeidler@gmail.com>
2026-03-25 14:22:29 +0100

    jakobs koder virker ikke med sælg

--- src/main/java/OrdreLinje.java
+++ src/main/java/OrdreLinje.java
@@ -1,43 +1,50 @@
+//public class OrdreLinje {
+//    private int antal;
+//    private Pizza pizza;
+//    private double pris;
+//    private int nr;
+//
+//
+//    public OrdreLinje(int antal, MenuLinje menuLinje) {
+//        this(antal, menuLinje.getPris(), menuLinje.getPizza(), menuLinje.getNr());
+//    }
+//
+//    public OrdreLinje(int antal, double pris, Pizza pizza, int nr){
+//        this.antal = antal;
+//        this.pris = pris;
+//        this.pizza = pizza;
+//        this.nr = nr;
+//    }
+//
+//    public void sælg(Menu menu) {
+//        for (MenuLinje menuLinje: menu.getMenuLinjer()) {
+//            if (menuLinje.getNr() == nr) menuLinje.sælg();
+//        }
+//    }
+//
+//
+//    public int getAntal() {
+//        return antal;
+//    }
+//
+//    public Pizza getPizza() {
+//        return pizza;
+//    }
+//
+//    public double getPris() {
+//        return pris * antal;
+//    }
+//
+//    public int getNr() {
+//        return nr;
+//    }
+//
+//    public String toString() {
+//        return String.format("%s x %s ... %s kr", antal, getNr(), getPris());
+//    }
+//}
+
 public class OrdreLinje {
-    private int antal;
-    private Pizza pizza;
-    private double pris;
-    private int nr;
-
-
-    public OrdreLinje(int antal, MenuLinje menuLinje) {
-        this(antal, menuLinje.getPris(), menuLinje.getPizza(), menuLinje.getNr());
-    }
-
-    public OrdreLinje(int antal, double pris, Pizza pizza, int nr){
-        this.antal = antal;
-        this.pris = pris;
-        this.pizza = pizza;
-        this.nr = nr;
-    }
-
-    public int getAntal() {
-        return antal;
-    }
-
-    public Pizza getPizza() {
-        return pizza;
-    }
-
-    public double getPris() {
-        return pris * antal;
-    }
-
-    public int getNr() {
-        return nr;
-    }
-
-    public String toString() {
-        return String.format("%s x %s ... %s kr", antal, getNr(), getPris());
-    }
-}
-
-/*    public class OrdreLinje {
     private int antal;
     private MenuLinje menuLinje;
 
@@ -66,4 +73,3 @@ public class OrdreLinje {
         return String.format("%s x %s ... %s kr", antal, menuLinje.getNr(), getPris());
     }
 }
-*/
